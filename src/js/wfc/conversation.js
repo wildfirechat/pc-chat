f1129d2125f9a187b85c94252fe055a08985e73f
imndx <imndxx@gmail.com>
2019-02-26 23:24:22 +0800

    no message

--- src/js/wfc/conversation.js
+++ src/js/wfc/conversation.js
@@ -10,4 +10,10 @@ export default class Conversation{
     conversationType = 0;
     target = '';
     line = 0;
+
+    constructor(type, target, line){
+        this.conversationType = type;
+        this.target = target;
+        this.line = line;
+    }
 }
