6e3f28b93560f0a55e94720f20949bd584d18ca2
imndx <imndxx@gmail.com>
2019-02-26 08:46:40 +0800

    no message

--- src/js/stores/wfc.js
+++ src/js/stores/wfc.js
@@ -3,6 +3,7 @@ import proto from 'node-loader!../../../node_modules/marswrapper.node';
 import TextMessageContent from '../wfc/messages/textMessageContent'
 import * as wfcMessage from '../wfc/messageConfig'
 import Message from '../wfc/messages/message';
+import Conversation from '../wfc/conversation';
 
 
 class WfcManager {
@@ -56,15 +57,27 @@ class WfcManager {
         // console.log(test.content);
         // console.log(test.base);
 
-        var json = '    { "conversation":{ "conversationType": 0, "target": "UZUWUWuu", "line": 0 }, "from": "UZUWUWuu", "content": { "type": 1, "searchableContent": "1234", "pushContent": "", "content": "", "binaryContent": "", "localContent": "", "mediaType": 0, "remoteMediaUrl": "", "localMediaPath": "", "mentionedType": 0, "mentionedTargets": [ ] }, "messageId": 52, "direction": 1, "status": 5, "messageUid": 75735276990792720, "timestamp": 1550849394256, "to": "" } ';
-        let msg = Object.assign(new Message(), JSON.parse(json));
-        let contentClazz = wfcMessage.getMessageContentClazz(msg.content.type);
-        let text = new contentClazz();
-        text.decode(msg.content);
-        console.log(text.content);
-        console.log(text instanceof TextMessageContent);
+        // var json = '    { "conversation":{ "conversationType": 0, "target": "UZUWUWuu", "line": 0 }, "from": "UZUWUWuu", "content": { "type": 1, "searchableContent": "1234", "pushContent": "", "content": "", "binaryContent": "", "localContent": "", "mediaType": 0, "remoteMediaUrl": "", "localMediaPath": "", "mentionedType": 0, "mentionedTargets": [ ] }, "messageId": 52, "direction": 1, "status": 5, "messageUid": 75735276990792720, "timestamp": 1550849394256, "to": "" } ';
+        // let msg = Object.assign(new Message(), JSON.parse(json));
+        // let contentClazz = wfcMessage.getMessageContentClazz(msg.content.type);
+        // let text = new contentClazz();
+        // text.decode(msg.content);
+        // console.log(text.content);
+        // console.log(text instanceof TextMessageContent);
         // console.log(msg.from);
         // console.log(msg.content);
+
+        let c1 = new Conversation();
+        c1.target = 'target';
+        c1.conversationType = 0;
+        c1.line = 0;
+
+        let c2 = new Conversation();
+        c2.target = 'target';
+        c2.conversationType = 0;
+        c2.line = 0;
+
+        console.log('conversation is same: ', _.isEqual(c1, c2));
     }
 
     /**
@@ -126,15 +139,13 @@ class WfcManager {
 
     /**
      * 
-     * @param {string} type 
-     * @param {string} target 
-     * @param {number} line 
+     * @param {Conversation} conversation
      * @param {number} fromIndex 
      * @param {boolean} before 
      * @param {number} count 
      * @param {string} withUser 
      */
-    @action async getMessageList(type, target, line, fromIndex, before, count, withUser){
+    @action async getMessageList(conversation, fromIndex, before, count, withUser){
         return [];
     }
 
@@ -152,6 +163,23 @@ class WfcManager {
 
     async sendTextMessage(type, target, line, to, text){
 
+    }
+
+    async sendMessage(message){
+        // let conv = {conversationType:0,target:'5O5J5JOO',line:0}
+        let strConv = JSON.stringify(message.conversation);
+        // let strCont = '{"type":1,"searchableContent":"hello","pushContent":"","content":"","binaryContent":"","localContent":"","mediaType":0,"remoteMediaUrl":"","localMediaPath":"","mentionedType":0,"mentionedTargets":[]}';
+        let strContt = JSON.stringify(message.payload);
+        let retValue = proto.sendMessage(strConv, strCont, "", 0, function(messageId, timestamp) { //preparedCB
+          console.log("sendMessage prepared:", messageId, timestamp);
+        }, function(uploaded, total) { //progressCB
+          console.log("sendMessage progress:", uploaded, total);
+        }, function(messageUid, timestamp) { //successCB
+          console.log("sendMessage success:", messageUid, timestamp);
+        }, function(errorCode) { //errorCB
+          console.log("sendMessage failed:", errorCode);
+        });
+        console.log("call sendMessage return:", retValue);
     }
 }
 const self = new WfcManager();
