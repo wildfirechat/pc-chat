01689fe0307c12f35374e75d6aed81c124229027
peaceme2019 <lovemi2019@163.com>
2019-05-28 23:59:11 +0800

    fix: 修复mac打包问题；待解决：dev运行时需手工拷贝相应平台的node到源码根目录

--- src/js/wfc/wfc.js
+++ src/js/wfc/wfc.js
@@ -1,4 +1,5 @@
 // import proto from 'node-loader!../../../node_modules/marswrapper.node';
+
 import proto from 'node-loader!../../../marswrapper.node';
 import Message from '../wfc/messages/message';
 import Conversation from '../wfc/model/conversation';
