895cfbe0cab607044f2b122f8a0114a4c567e8f8
peaceme2019 <lovemi2019@163.com>
2019-06-06 10:28:39 +0800

    fix: win用自定义标题栏，添加控制按钮

--- src/js/pages/Header/index.js
+++ src/js/pages/Header/index.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react';
+import { remote, ipcRenderer } from 'electron';
 
 import classes from './style.css';
 
@@ -17,10 +18,37 @@ export default class Header extends Component {
         }
     }
 
+    // 关闭窗口
+    close() {
+        ipcRenderer.send('close-window');
+    }
+    // 最小化窗口
+    min() {
+        ipcRenderer.send('min-window');
+    }
+    // 切换窗口状态：如果当前状态是最大化则取消最大化，否则最大化
+    toggle() {
+        ipcRenderer.send('toggle-max');
+    }
+
     render() {
+        var isWin = window.process.platform === 'win32';
         return (
             <header className={classes.container}>
                 <h1>{this.getTitle()}</h1>
+                 {
+                    (isWin) && (
+                        <div>
+
+                            <p onClick={e => this.min()}>-</p>
+
+                            <p onClick={e => this.toggle()}>口</p>
+
+                            <p onClick={e => this.close()}>X</p>
+
+                        </div>
+                    )
+                 }
             </header>
         );
     }
