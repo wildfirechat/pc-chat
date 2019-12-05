d98fcb80e5e2f350fa72df627ebdca1a20d88817
x86 <1960867973@qq.com>
2019-12-05 19:07:13 +0800

    fix media mesage content

--- src/js/wfc/messages/mediaMessageContent.js
+++ src/js/wfc/messages/mediaMessageContent.js
@@ -13,7 +13,7 @@ export default class MediaMessageContent extends MessageContent {
         if (file) {
             this.localPath = file.path;
             // attention: 粘贴的时候，path是空字符串，故采用了这个trick
-            if (this.localPath.indexOf(file.name) < 0) {
+            if (this.localPath && this.localPath.indexOf(file.name) < 0) {
                 this.localPath += file.name;
             }
         }
