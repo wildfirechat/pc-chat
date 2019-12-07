45bf669b06ca8ebd668f5779a510489fab7545c1
x86 <1960867973@qq.com>
2019-12-07 09:32:44 +0800

    refactor

--- src/voip.js
+++ src/voip.js
@@ -1,16 +1,12 @@
 
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 import { Provider } from 'mobx-react';
-import { HashRouter } from 'react-router-dom';
-import { ipcRenderer, remote } from 'electron';
 
 import './global.css';
 import './assets/fonts/icomoon/style.css';
 import 'utils/albumcolors';
-import getRoutes from './js/ui/routes';
 import stores from './js/ui/stores';
-import Voip from './js/ui/pages/Voip';
+import Voip from './js/ui/components/Voip';
 
 export default class VoipApp extends Component {
     async componentWillMount() {
