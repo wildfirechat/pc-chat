992af52a78d797e689971479595fa0d7b7fe54e9
x86 <1960867973@qq.com>
2019-12-12 14:43:45 +0800

    disable voip in group conversation.

--- src/js/ui/components/MessageInput/index.js
+++ src/js/ui/components/MessageInput/index.js
@@ -336,6 +336,7 @@ export default class MessageInput extends Component {
 
     render() {
         var canisend = this.canisend();
+        let canStartVoip = this.props.conversation && this.props.conversation.type === ConversationType.Single;
 
         return (
             <div
@@ -375,13 +376,13 @@ export default class MessageInput extends Component {
                     <i
                         className="icon-ion-android-camera"
                         id="videoCall"
-                        onClick={e => canisend && this.videoCall()}
+                        onClick={e => canisend && canStartVoip && this.videoCall()}
                     />
 
                     <i
                         className="icon-ion-ios-telephone"
                         id="audioCall"
-                        onClick={e => canisend && this.audioCall()}
+                        onClick={e => canisend && canStartVoip && this.audioCall()}
                     />
 
                     <i
