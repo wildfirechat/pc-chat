6156774ec1c9f4dd97bc15e03586df99a8265e2f
x86 <1960867973@qq.com>
2020-02-18 23:41:33 +0800

    add multi call layout

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import ViewManager from './js/ui/viewManager';
+import Voip from "./js/ui/pages/Voip/multi";
 
-ReactDOM.render(<ViewManager />, document.getElementById('root'));
+ReactDOM.render(<Voip/>, document.getElementById('root'));
