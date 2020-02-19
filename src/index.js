d67f403ad546716d6afbe36dce88dcfb46d821ef
x86 <1960867973@qq.com>
2020-02-19 14:58:43 +0800

    fix start up issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import ViewManager from './js/ui/viewManager';
-import Voip from "./js/ui/pages/Voip/multi";
 
-ReactDOM.render(<Voip/>, document.getElementById('root'));
+ReactDOM.render(<ViewManager/>, document.getElementById('root'));
