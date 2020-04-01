69ebe437d86d16a4941a5447158c298668fc9ebd
x86 <1960867973@qq.com>
2020-04-01 18:01:35 +0800

    sync with web-chat

--- src/js/config.js
+++ src/js/config.js
@@ -1,14 +1,16 @@
 import { isElectron } from './platform'
 export default class Config {
+    // 是否支持多人音视频通话
+    static ENABLE_MULTI_VOIP_CALL = false;
+    // 是否支持1对1音视频通话
+    static ENABLE_SINGLE_VOIP_CALL = true;
     static DEFAULT_PORTRAIT_URL = 'https://static.wildfirechat.cn/user-fallback.png';
-    static APP_SERVER = 'http://wildfirechat.cn:8888';
+    static APP_SERVER = 'http://192.168.31.113:8888';
     static QR_CODE_PREFIX_PC_SESSION = "wildfirechat://pcsession/";
     static ICE_ADDRESS = 'turn:turn.wildfirechat.cn:3478';
     static ICE_USERNAME = 'wfchat';
     static ICE_PASSWORD = 'wfchat';
     static LANGUAGE = 'zh_CN';
-
-    static ENABLE_MULTI_CALL = true;
 
     static getWFCPlatform() {
         if (isElectron()) {
