30165f69573668eec27dd302471078613b70a9a6
x86 <1960867973@qq.com>
2020-06-22 17:26:58 +0800

    fix quit group issue

--- src/js/ui/pages/Members/index.js
+++ src/js/ui/pages/Members/index.js
@@ -79,7 +79,7 @@ import MessageConfig from '../../../wfc/client/messageConfig';
     },
     quitGroup: async () => {
         wfc.quitGroup(stores.chat.target.target, [0], null, (e) => {
-
+            stores.members.toggle(false)
         }, (e) => {
             console.warn(e);
         })
