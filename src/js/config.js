e984fbc0c9df70bb73a378bc25f2fe282b579d85
x86 <1960867973@qq.com>
2020-07-23 22:37:00 +0800

    update avenginekit and config

--- src/js/config.js
+++ src/js/config.js
@@ -6,6 +6,8 @@ export default class Config {
     static ENABLE_SINGLE_VOIP_CALL = true;
     // 打开voip调试模式时，voip window不会自动关闭，方便分析控制台日志，需要手动关闭。
     static ENABLE_VOIP_DEBUG_MODE = false;
+    // 挂断音视频通话后，音视频通话窗口延时多久关闭，单位是秒。
+    static VOIP_WINDOW_CLOSE_DELAY = 2;
 
     static DEFAULT_PORTRAIT_URL = 'https://static.wildfirechat.cn/user-fallback.png';
     static APP_SERVER = 'http://wildfirechat.cn:8888';
