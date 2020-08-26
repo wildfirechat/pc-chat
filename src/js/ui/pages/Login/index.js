98708d24fdbb910d9b4ab0339e1cba10ba7fe2e8
imndx <imndxx@gmail.com>
2020-08-26 23:10:19 +0800

    PC端直接登录

--- src/js/ui/pages/Login/index.js
+++ src/js/ui/pages/Login/index.js
@@ -1,15 +1,14 @@
-
-import React, { Component } from 'react';
-import { observer, inject } from 'mobx-react';
+import React, {Component} from 'react';
+import {observer, inject} from 'mobx-react';
 
 import classes from './style.css';
 import Config from '../../../config';
 import jrQRCode from 'jr-qrcode'
 import wfc from '../../../wfc/client/wfc'
 import PCSession from '../../../wfc/model/pcsession';
-import { observable } from 'mobx';
+import {observable} from 'mobx';
 import axios from 'axios';
-import { connect } from '../../../platform'
+import {connect} from '../../../platform'
 
 @inject(stores => ({
     avatar: stores.sessions.avatar,
@@ -27,9 +26,16 @@ export default class Login extends Component {
     componentDidMount() {
         axios.defaults.baseURL = Config.APP_SERVER;
 
-        this.getCode();
-        // this.keepLogin();
-        this.refreshQrCode();
+        let userId = localStorage.getItem("userId");
+        this.createPCLoginSession(userId);
+        if (!userId) {
+            // 此前尚未登录过，需要手机端扫码登录
+            this.refreshQrCode();
+        } else {
+            // 此前已登录过，直接点登录，请求手机端确认登录
+            // TODO 渲染头像 按钮等
+        }
+        // TODO 渲染切换账号，并且点击切换账号的时候，localStorage.setItem('userId', '')
     }
 
     componentWillUnmount() {
@@ -44,7 +50,7 @@ export default class Login extends Component {
                 {
                     <img
                         className="disabledDrag"
-                        src={this.props.avatar} />
+                        src={this.props.avatar}/>
                 }
 
                 <p>Scan successful</p>
@@ -53,19 +59,23 @@ export default class Login extends Component {
         );
     }
 
-    async getCode() {
-        var response = await axios.post('/pc_session', {
-            token: this.appToken,
+    async createPCLoginSession(userId) {
+        let response = await axios.post('/pc_session', {
+            userId: userId,
             device_name: 'pc',
             clientId: wfc.getClientId(),
             platform: Config.getWFCPlatform()
         });
-        console.log('----------- getCode', response.data);
+        console.log('----------- createPCLoginSession', response.data);
         if (response.data) {
             let session = Object.assign(new PCSession(), response.data.result);
             this.appToken = session.token;
-            this.qrCode = jrQRCode.getQrBase64(Config.QR_CODE_PREFIX_PC_SESSION + session.token);
-            this.desc = '扫码登录野火IM'
+            if (!userId) {
+                this.qrCode = jrQRCode.getQrBase64(Config.QR_CODE_PREFIX_PC_SESSION + session.token);
+                this.desc = '扫码登录野火IM'
+            } else {
+                // TODO 渲染请求已发送
+            }
             this.login();
         }
     }
@@ -82,14 +92,8 @@ export default class Login extends Component {
             this.getCode();
         }, 30 * 1000);
     }
-    async login() {
-        if(sessionStorage.getItem('token') && sessionStorage.getItem('userId')){
-            connect(sessionStorage.getItem('userId'), sessionStorage.getItem('token'));
-            WildFireIM.config.loginUser= wfc.getUserInfo(wfc.getUserId());
-            WildFireIM.config.token= this.token;
-            return;
-        }
 
+    async login() {
         if (this.appToken === '' || this.lastAppToken === this.appToken) {
             console.log('-------- token is empty or invalid');
             return;
@@ -103,11 +107,11 @@ export default class Login extends Component {
                     let userId = response.data.result.userId;
                     let imToken = response.data.result.token;
                     connect(userId, imToken);
-                    WildFireIM.config.loginUser= wfc.getUserInfo(wfc.getUserId());
+                    WildFireIM.config.loginUser = wfc.getUserInfo(wfc.getUserId());
                     WildFireIM.config.token = this.appToken;
 
-                    sessionStorage.setItem('token', token);
-                    sessionStorage.setItem('userId', userId);
+                    localStorage.setItem('userId', userId);
+                    // TODO store portrait etc
                     break;
                 case 9:
                     console.log('qrcode scaned', response.data);
@@ -121,7 +125,7 @@ export default class Login extends Component {
                     console.log(response.data);
                     break
             }
-           
+
         }
     }
 
@@ -130,7 +134,7 @@ export default class Login extends Component {
         return (
             <div className={classes.inner}>
                 {
-                    this.qrCode && (<img className="disabledDrag" src={this.qrCode} />)
+                    this.qrCode && (<img className="disabledDrag" src={this.qrCode}/>)
                 }
 
                 <a href={window.location.pathname + '?' + +new Date()}>刷新二维码</a>
