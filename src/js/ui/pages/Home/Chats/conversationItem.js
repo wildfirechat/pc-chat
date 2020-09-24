7e7abe9bb6ff2f1c89b96173ba22e368073b6c22
imndx <imndxx@gmail.com>
2020-09-24 12:20:36 +0800

    fix unread issue

--- src/js/ui/pages/Home/Chats/conversationItem.js
+++ src/js/ui/pages/Home/Chats/conversationItem.js
@@ -178,6 +178,7 @@ export default class ConversationItem extends Component {
                                     <span
                                         className={classes.message}
                                         dangerouslySetInnerHTML={{__html: e.draft ? '[草稿]' + e.draft : (e.lastMessage && e.lastMessage.messageContent ? e.lastMessage.messageContent.digest(e.lastMessage) : '')}}/>
+                                    {e.isSilent ? isSilent : ''}
                                 </div>
                             </div>
 
